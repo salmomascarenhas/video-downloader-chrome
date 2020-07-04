b5a877cd148c1f28f5aea25efc05174c57e3de88
Salmo <salmo.cruz@gmail.com>
2020-07-03 22:45:35 -0300

    fix erros

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,23 +1,33 @@
 let data = [];
 
-
-function anyfunction(request) {
-    if (request.from === 'content' && request.subject === 'dataRequested') {
-        data = request.data;
-        alert('dados armazenados no popup com sucesso!');
-        alert(data);
-    }
-}
-
-document.querySelector('#btn').addEventListener('click', () => {
+document.addEventListener('DOMContentLoaded', () => {
     chrome.tabs.query({
         active: true,
         currentWindow: true
     }, tabs => {
-        alert('dados solicitado do content com sucesso!')
+        // alert('dados solicitado do content com sucesso!')
         chrome.tabs.sendMessage(
             tabs[0].id,
             { from: 'popup', subject: 'sendDataToPopUp' },
             anyfunction);
     });
 });
+
+function anyfunction(request) {
+    if (request.from === 'content' && request.subject === 'dataRequested') {
+        const container = document.querySelector('.container');
+        data = request.data;
+        console.log('dados armazenados no popup com sucesso!');
+
+        if (data.lenght) {
+
+        }
+        data.forEach((element, index) => {
+            container.innerHTML +=
+                `<div class='card'>
+                    <a href="${element}" target="_blank">link ${index}</a>
+                </div>`;
+
+        });
+    }
+}
